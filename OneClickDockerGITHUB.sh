b6050a8cddc46ab09316ba01a85f97b7f62e14c2
CodeMonk <85857368+anmolpal@users.noreply.github.com>
2021-11-03 12:49:50 +0530

    Update OneClickDockerGITHUB.sh

--- OneClickDockerGITHUB.sh
+++ OneClickDockerGITHUB.sh
@@ -1,52 +1,3 @@
-#Shell_Script_for_Docker
-
-read -t 5 -p "<<<<<<<<<<<<<<<<< Creating User >>>>>>>>>>>>>>>>>>>>>>>>>>> "
-echo " "
-RED='\033[0;31m'
-echo -e "${RED}Do you want to create a new user to install Docker or you want to install Docker in the current user? [yes/no]"
-NC='\033[0m'
-echo -e "${NC}"
-read x
-if [[ "${x}" = "yes" ]]
-then
-    read -p "Enter new username: " userInput
-    cd /home
-    if [[ -d "$userInput" ]]
-    then
-        GREEN='\033[0;42m'
-        echo -e "${GREEN}User Exists"
-        NC='\033[0m'
-        echo -e "${NC}"
-    else 
-        echo "User does not exist. Creating now... "
-        sudo adduser $userInput
-        read -p "To give privileges to the user you have created enter username:"
-        sudo usermod -aG sudo $userInput
-        cd /home/$userInput
-        sudo wget https://github.com/anmolpal/OneClickDockerGITHUB/archive/refs/heads/main.zip
-        sudo unzip main.zip
-    fi    
-else
-    echo 'Skipped New User Creation'
-fi
-
-
-read -t 5 -p "<<<<<<<<<<<<<<<<< Loging into New User >>>>>>>>>>>>>>>>>>>>>>>>>>> "
-echo " "
-RED='\033[0;31m'
-echo -e "${RED}Do you want to login into New User or stay in Current User [yes/no]"
-NC='\033[0m'
-echo -e "${NC}"
-read x
-if [[ "${x}" = "yes" ]]
-then
-    echo " "
-    read -p "Login into your newly created user:"
-    su $REPLY
-else
-    echo "Continuing with Current User"
-fi    
-
 
 #To delete a user
 #sudo deluser username
@@ -79,100 +30,71 @@ sudo systemctl start docker
 sudo apt-get update
 sudo apt-get upgrade
 
-
 read -t 5 -p "<<<<<<<<<<<<<<<<< Deploying Images >>>>>>>>>>>>>>>>>>>>>>>> "
 echo " "
 RED='\033[0;31m'
 echo -e "${RED}Which image do you want to deploy?"
 NC='\033[0m'
 echo -e "${NC}"
-echo "spark"
-echo "hadoop"
-echo "postgresql"
-read  x
-if [[ "${x}" = "spark" ]]
-then
-    docker pull bitnami/spark
-    sudo apt-get install unzip
-    cd /usr/local
-    sudo wget https://github.com/anmolpal/Docker-Compose/archive/refs/heads/main.zip
-    sudo unzip main.zip 
-    cd /usr/local/Docker-Compose-main/ 
-    sudo chmod 777 docker-compose.yml
-    sudo apt install docker-compose
-    docker-compose up -d
-    cd /usr/local/
-    sudo rm -rf main.zip
-
-elif [[ "${x}" = "hadoop" ]]
-then
-    docker pull lewuathe/hadoop-master
-    sudo apt-get install unzip
-    cd /usr/local
-    sudo wget https://github.com/anmolpal/Hadoop-Docker-Compose/archive/refs/heads/main.zip
-    sudo unzip main.zip 
-    cd /usr/local/Hadoop-Docker-Compose-main/
-    sudo chmod 777 docker-compose.yml
-    sudo apt install docker-compose
-    docker-compose up -d
-    cd /usr/local/ 
-    sudo rm -rf main.zip
-
-elif [[ "${x}" = "postgresql" ]] 
-then
-    docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-        #statements    
-else
-    echo "No selection choosen"
-fi 
-
-
-echo " "
-RED='\033[0;31m'
-echo -e "${RED}Do you want to Deploy another Docker Image? [yes/no]"
-NC='\033[0m'
-echo -e "${NC}"
-read  x
-if [[ "${x}" = "yes" ]]
-then
-    read -t 5 -p "<<<<<<<<<<<<<<<<< Deploying Images >>>>>>>>>>>>>>>>>>>> "
-    echo "Which image do you want to deploy?"
-    echo "spark"
-    echo "hadoop"
-    read  x
-    if [[ "${x}" = "spark" ]]
-    then
-        docker pull bitnami/spark
-        sudo apt-get install unzip
-        cd /usr/local
-        sudo wget https://github.com/anmolpal/Docker-Compose/archive/refs/heads/main.zip
-        sudo unzip main.zip 
-        cd /usr/local/Docker-Compose-main/ 
-        sudo chmod 777 docker-compose.yml
-        sudo apt install docker-compose
-        docker-compose up -d
-        cd /usr/local/
-        sudo rm -rf main.zip
-
-    elif [[ "${x}" = "hadoop" ]]
-    then
-        docker pull lewuathe/hadoop-master
-        sudo apt-get install unzip
-        cd /usr/local
-        sudo wget https://github.com/anmolpal/Hadoop-Docker-Compose/archive/refs/heads/main.zip
-        sudo unzip main.zip 
-        cd /usr/local/Hadoop-Docker-Compose-main/
-        sudo chmod 777 docker-compose.yml
-        sudo apt install docker-compose
-        docker-compose up -d
-        cd /usr/local/ 
-        sudo rm -rf main.zip
-    
-    else
-        echo "No selection choosen"
-    fi
-else
-    echo "No selection choosen"
-fi 
+PS3='Please enter your choice: '
+options=("1 Hadoop" "2 Spark" "3 Postgresql" "4 Airflow" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "1 Hadoop")
+            echo "you chose choice 1"
+            docker pull lewuathe/hadoop-master
+            sudo apt-get install unzip
+            cd /usr/local
+            sudo wget https://github.com/anmolpal/Hadoop-Docker-Compose/archive/refs/heads/main.zip
+            sudo unzip main.zip 
+            cd /usr/local/Hadoop-Docker-Compose-main/
+            sudo chmod 777 docker-compose.yml
+            sudo apt install docker-compose
+            docker-compose up -d
+            cd /usr/local/ 
+            sudo rm -rf main.zip
+            ;;
+        "2 Spark")
+            echo "you chose choice 2"
+            docker pull bitnami/spark
+            sudo apt-get install unzip
+            cd /usr/local
+            sudo wget https://github.com/anmolpal/Docker-Compose/archive/refs/heads/main.zip
+            sudo unzip main.zip 
+            cd /usr/local/Docker-Compose-main/ 
+            sudo chmod 777 docker-compose.yml
+            sudo apt install docker-compose
+            docker-compose up -d
+            cd /usr/local/
+            sudo rm -rf main.zip
+            ;;
+        "3 Postgresql")
+            echo "you chose choice $REPLY which is $opt"
+            docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+            ;;
+        "4 Airflow")
+            echo "you chose choice $REPLY which is $opt"
+            #Recommended docker-compose version 1.29 or above. If version is less then 1.29 uninstall 
+            #previous docker-compose
+            #pip uninstall docker-compose
+            sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            sudo chmod +x /usr/local/bin/docker-compose
+            sudo mkdir airflow-docker
+            sudo chmod 777 airflow-docker
+            cd airflow-docker
+            curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.2.1/docker-compose.yaml'
+            sudo mkdir ./dags ./plugins ./logs
+            sudo chmod 777 ./dags ./plugins ./logs
+            echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+            docker-compose up airflow-init
+            docker-compose up -d
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 
 
